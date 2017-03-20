a17f418cbc75503f7fd5e390c272bbc81d93844a
ChrisM90 <Chris.Mortimer127654@hotmail.com>
2017-03-20 13:11:10 +0000

    Changes to ROM Access

--- Arduino/TeensyBoy/Arm7.cpp
+++ Arduino/TeensyBoy/Arm7.cpp
@@ -583,6 +583,16 @@ uint32_t Processor::ReadNop32(uint32_t address)
 uint8_t Processor::ReadROM8(uint32_t address, uint8_t bank)
 {
   waitCycles += bankSTimes[(address >> 24) & 0xF];
+  
+  if(bank == 1)
+  {
+    address = (address & romBank1Mask);
+  }
+  else if(bank == 2)
+  {
+    address = (address & romBank2Mask) + romBank1Mask;
+  }
+  
   ROM->seek(address);
   return ROM->read();
 }
@@ -590,6 +600,16 @@ uint8_t Processor::ReadROM8(uint32_t address, uint8_t bank)
 uint16_t Processor::ReadROM16(uint32_t address, uint8_t bank)
 {
   waitCycles += bankSTimes[(address >> 24) & 0xF];
+
+  if(bank == 1)
+  {
+    address = (address & romBank1Mask);
+  }
+  else if(bank == 2)
+  {
+    address = (address & romBank2Mask) + romBank1Mask;
+  }
+  
   ROM->seek(address);
   return (uint16_t)(ROM->read() | (ROM->read() << 8));
 }
@@ -597,6 +617,16 @@ uint16_t Processor::ReadROM16(uint32_t address, uint8_t bank)
 uint32_t Processor::ReadROM32(uint32_t address, uint8_t bank)
 {
   waitCycles += (bankSTimes[(address >> 24) & 0xF] * 2) + 1;
+
+  if(bank == 1)
+  {
+    address = (address & romBank1Mask);
+  }
+  else if(bank == 2)
+  {
+    address = (address & romBank2Mask) + romBank1Mask;
+  }
+  
   ROM->seek(address);
   return (uint32_t)(ROM->read() | (ROM->read() << 8) | (ROM->read() << 16) | (ROM->read() << 24));
 }
@@ -1995,7 +2025,7 @@ void Processor::LoadCartridge()
     return;
   }
 
-  if (cartSize > 1 << 24) //Two Banks
+  if (cartSize > (1 << 24)) //Two Banks
   {
     romBank1Mask = (1 << 24) - 1;
     romBank2Mask = (1 << 24) - 1;
@@ -2014,6 +2044,26 @@ void Processor::LoadCartridge()
   {
     RomBankCount = 2;
   }
+
+  //Address Bytes Expl.
+  //000h    4     ROM Entry Point  (32bit ARM branch opcode, eg. "B rom_start")
+  //004h    156   Nintendo Logo    (compressed bitmap, required!)
+  //0A0h    12    Game Title       (uppercase ascii, max 12 characters)
+  //0ACh    4     Game Code        (uppercase ascii, 4 characters)
+  //0B0h    2     Maker Code       (uppercase ascii, 2 characters)
+  //0B2h    1     Fixed value      (must be 96h, required!)
+  //0B3h    1     Main unit code   (00h for current GBA models)
+  //0B4h    1     Device type      (usually 00h)
+  //0B5h    7     Reserved Area    (should be zero filled)
+  //0BCh    1     Software version (usually 00h)
+  //0BDh    1     Complement check (header checksum, required!)
+  //0BEh    2     Reserved Area    (should be zero filled)
+  //--- Additional Multiboot Header Entries ---
+  //0C0h    4     RAM Entry Point  (32bit ARM branch opcode, eg. "B ram_start")
+  //0C4h    1     Boot mode        (init as 00h - BIOS overwrites this value!)
+  //0C5h    1     Slave ID Number  (init as 00h - BIOS overwrites this value!)
+  //0C6h    26    Not used         (seems to be unused)
+  //0E0h    4     JOYBUS Entry Pt. (32bit ARM branch opcode, eg. "B joy_start")
 }
 
 uint8_t Processor::ReadU8Funcs(uint16_t bank, uint32_t address)
